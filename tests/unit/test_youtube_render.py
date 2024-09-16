27caaa3a3fdb25865504e76358464948cde0eb6a
Manjunath-Bhashyam <manjunathbhashyam1995@gmail.com>
2024-09-16 19:11:05 +0530

    formatted with black

--- tests/unit/test_youtube_render.py
+++ tests/unit/test_youtube_render.py
@@ -13,13 +13,14 @@ URL_id_bad_data = [
     ("https://www.youtube.com/watch?v=roO5VGxOw2sahesbf"),  # exception
     ("https://www.youtube.com/watch?v=roO5VGxOw2s&t"),  # exception
     ("https://www.youtube.com/watch?v=roO5VGxOw2s&t==22s"),  # exception
-    ("https://www.youtube.com/watch?v==roO5VGxOw2s&t=22s")
+    ("https://www.youtube.com/watch?v==roO5VGxOw2s&t=22s"),
 ]
 
 
 @pytest.mark.parametrize("URL, response", good_URL_data)
 def test_get_time_info(URL, response):
     assert get_time_info(URL) == response
+
 
 @pytest.mark.parametrize("URL", URL_id_bad_data)
 def test_get_time_info_failed(URL):
